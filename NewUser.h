4d4061a4676ea1d0f2fe208df76d921420d5a1e1
Alexander Ocheretin <mobidev@yandex.ru>
2024-07-04 14:12:26 +0300

    Подправил в файле интерфейса NewUser описание виртуальной функции Show

--- NewUser.h
+++ NewUser.h
@@ -8,10 +8,7 @@ public:
 	 NewUser() = default;
 	 NewUser(const string& acc) : _acc(acc) {}
 	virtual ~NewUser() = default;
-	virtual void Show()=0
-	{
-		
-	}
+	virtual void Show() = 0;
 	const  string get_Acc() const;
 	void set_Acc(const string acc);
 	
