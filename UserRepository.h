f2a9622e3332582e8c63cd1823f74c8964416f5d
Alexander Ocheretin <mobidev@yandex.ru>
2024-07-17 12:58:17 +0300

    Добавлено описание UserReposiory в README, комментарии, лог работы

--- UserRepository.h
+++ UserRepository.h
@@ -8,8 +8,8 @@ class UserRepository
 	vector<Acc*> _users; // Вектор для хранения всех пользователей
 public:  UserRepository();
 		Acc* NewUser();
-		Acc* FindUser(string login);
-		Acc* AuthorizeUser(string login, string pwd);
+		Acc* FindUser(string login); // Поиск логина пользователя, существует ли пользователь в векторе пользователей
+		Acc* AuthorizeUser(string login, string pwd); // Попытка авторизации пользователя
 		int getSize(); // Получить количество зарегистрированных пользователей
 		void UsersList(void); // Вывод списка зарегистрированных пользователей
 		~UserRepository();
